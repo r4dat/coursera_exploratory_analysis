559f90d4162559ba736982c43d46c32d618e2aae
r4dat <rfourtoss@gmail.com>
2014-06-04 11:07:31 -0500

    Add test for file exist, if not download and unzip.
    Do date conversion and subset to feb 01,02 2007.

--- get_data.R
+++ get_data.R
@@ -1,3 +1,7 @@
+
+#Fetch and unzip file if it doesn't exist.
+if(!file_test(op="-f",x="household_power_consumption.txt")){
+
 # create a temporary file and a temporary directory on your local disk
 tf <- tempfile()
 td <- tempdir()
@@ -11,3 +15,17 @@ download.file(
 
 # unzip the files to the temporary directory
 files <- unzip( tf , exdir = getwd() )
+}
+
+require(lubridate)
+require(dplyr)
+
+full_power = read.csv(
+  "C:/Users/rrutherf/Desktop/RStudio/WorkDirectory/
+  coursera_exploratory_analysis/household_power_consumption.txt",
+  sep=";",stringsAsFactors=FALSE,na.strings="?")
+
+full_power$Date=dmy(full_power$Date)
+subpower=filter(full_power,
+                Date>=mdy("02012007"),Date<=mdy("02022007"))
+
