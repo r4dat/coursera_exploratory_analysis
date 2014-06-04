2f9d612d29b442af47d272bf6b1bf4e04f2357ad
r4dat <rfourtoss@gmail.com>
2014-06-04 12:08:11 -0500

    Issue was image size, downscale to 480x480 makes bars past 6 basically vanish.

--- plot1.R
+++ plot1.R
@@ -2,15 +2,8 @@
 #Assume subsetted data exists already called "subsetted".
 
 
-# oddities: xlim limits our x-range and removes values > xlim.
-# However it creates 0-6 x-axis, which is removed by xaxt parameter.
 hist(x=subsetted$Global_active_power,
      xlab="Global Active Power (kilowatts)",
-     col="red",main="Global Active Power",
-     xlim=range(0,6),xaxt="n")
+     col="red",main="Global Active Power")
 
-# To create desired 0,2,4,6 axis we manually create it below.
-axis(side=1,at=seq(0,6,2),labels=seq(0,6,2))
-
-## NOT QUITE RIGHT, X AXIS limited, but sample pic 
-## shows no bars?
+png(filename="Plot1.png",width=480,height=480,units="px",type="cairo-png")
