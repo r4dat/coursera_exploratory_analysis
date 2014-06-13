8ebbceaad3621a2ff8ca98e5c4f77520e5d44367
r4dat <rfourtoss@gmail.com>
2014-06-13 13:09:16 -0500

    Add barplot initial.

--- pollution_analysis.R
+++ pollution_analysis.R
@@ -3,3 +3,8 @@ source(get_data.R)
 
 summary_dat = ddply(NEI,.(year),summarize,ToTEmissions=sum(Emissions))
 
+## initial barplot, not vert axis change yet.
+barplot(summary_dat$ToTEmissions,names=summary_dat$year,
+        xlab="Year",ylab="Total Emissions (Tons)",
+        main="Total Yearly Emissions",
+        col="red")
