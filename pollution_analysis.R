71a26684d8fb3e3ee1f75375901dbdc3546290c6
r4dat <rfourtoss@gmail.com>
2014-06-13 13:20:18 -0500

    df name change, add baltimore subsetting.

--- pollution_analysis.R
+++ pollution_analysis.R
@@ -1,10 +1,16 @@
 require(plyr)
 source(get_data.R)
 
-summary_dat = ddply(NEI,.(year),summarize,ToTEmissions=sum(Emissions))
+totSumm = ddply(NEI,.(year),summarize,ToTEmissions=sum(Emissions))
 
-## initial barplot, not vert axis change yet.
-barplot(summary_dat$ToTEmissions/1000,names=summary_dat$year,
+## initial barplot, not vert axis change yet. Total country wide.
+barplot(totSumm$ToTEmissions/1000,names=totSumm$year,
         xlab="Year",ylab="Total Emissions (1000's of Tons)",
         main="Yearly PM2.5 Emissions",
         col="red")
+
+# Check baltimore city Maryland, FIPS 24510
+
+baltSumm = ddply(NEI[NEI$fips=="24510",],.(year),
+                 summarize,ToTEmissions=sum(Emissions))
+
