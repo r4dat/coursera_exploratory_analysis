c2d055872f7db6182545bef5e4fde1b5d3e59e7c
r4dat <rfourtoss@gmail.com>
2014-06-13 13:16:36 -0500

    Relabel and change scale of y-axis on barplot.

--- pollution_analysis.R
+++ pollution_analysis.R
@@ -4,7 +4,7 @@ source(get_data.R)
 summary_dat = ddply(NEI,.(year),summarize,ToTEmissions=sum(Emissions))
 
 ## initial barplot, not vert axis change yet.
-barplot(summary_dat$ToTEmissions,names=summary_dat$year,
-        xlab="Year",ylab="Total Emissions (Tons)",
-        main="Total Yearly Emissions",
+barplot(summary_dat$ToTEmissions/1000,names=summary_dat$year,
+        xlab="Year",ylab="Total Emissions (1000's of Tons)",
+        main="Yearly PM2.5 Emissions",
         col="red")
