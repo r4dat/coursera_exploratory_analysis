f15fd707aedd1e0dc69fbff17092bad632890baa
r4dat <rfourtoss@gmail.com>
2014-06-13 13:57:03 -0500

    Remove old plot code. See ExData clone.

--- plot1_base.R
+++
@@ -1,9 +0,0 @@
-#attempt to do plot 1
-#Assume subsetted data exists already called "subsetted".
-
-
-hist(x=subsetted$Global_active_power,
-     xlab="Global Active Power (kilowatts)",
-     col="red",main="Global Active Power")
-
-png(filename="plot1.png",width=480,height=480,units="px",type="cairo-png")
