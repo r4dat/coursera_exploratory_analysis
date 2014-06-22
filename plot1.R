d15370c6f6201217a8bceb169679d9c1c79bae1f
“r4dat” <rfourtoss@gmail.com>
2014-06-22 16:55:25 -0500

    Start moving into individual plot files.

---
+++ plot1.R
@@ -0,0 +1,12 @@
+source("get_data.R")
+
+totSumm = ddply(NEI,.(year),summarize,ToTEmissions=sum(Emissions))
+
+png(filename="plot1.png",width=400,height=400,units = px)
+
+## initial barplot, not vert axis change yet. Total country wide.
+barplot(totSumm$ToTEmissions/1000,names=totSumm$year,
+        xlab="Year",ylab="Total Emissions (1000's of Tons)",
+        main="Yearly PM2.5 Emissions",
+        col="red")
+dev.off()
