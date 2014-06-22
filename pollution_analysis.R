55f69ef238f7e8c38f77773864940a232205fef6
“r4dat” <rfourtoss@gmail.com>
2014-06-22 16:55:40 -0500

    Add question 4 section.

--- pollution_analysis.R
+++ pollution_analysis.R
@@ -1,6 +1,5 @@
 require(plyr)
 require(ggplot2)
-require(sqldf)
 source("get_data.R")
 
 totSumm = ddply(NEI,.(year),summarize,ToTEmissions=sum(Emissions))
@@ -36,3 +35,28 @@ ggplot(data=baltsplit, aes(x=year,y=TotEmissions))+geom_bar(stat="identity")+
 
 ## 4 Across the United States, how have emissions from coal combustion-related 
 ## sources changed from 1999–2008?
+
+## Create subset of SCC id's with coal and lignite (brown coal) (and not charcoal.)
+## Return line if LevelThree is (coal OR lignite AND NOT charcoal)
+coalIndex = which( 
+                  (grepl(pattern = "coal",SCC$SCC.Level.Three,ignore.case=TRUE)
+                  | 
+                  grepl(pattern = "lignite",SCC$SCC.Level.Three,ignore.case=TRUE))
+                  &
+                  !grepl(pattern="Charcoal",SCC$SCC.Level.Three,ignore.case=TRUE)
+                  )
+
+coalSCC = SCC[coalIndex,1]
+
+coalNEI = subset(NEI, SCC %in% coalSCC)
+
+coalTotEm = aggregate(Emissions ~ year, data=coalNEI, sum)
+
+# Plot coal emissions.
+
+barplot(coalTotEm$Emissions/1000,names=coalTotEm$year,
+        xlab="Year",ylab="Total Emissions (1000's of Tons)",
+        main="PM2.5 Emissions from Coal Combustion. and related sources",
+        col="green")
+
+## 5
